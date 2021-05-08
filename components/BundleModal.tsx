863c4a74e72abb61f15f005c0cceab90042133b7
Martín Triay <martriay@gmail.com>
2021-05-08 01:30:25 -0300

    flush url on modal close

--- components/BundleModal.tsx
+++ components/BundleModal.tsx
@@ -1,9 +1,15 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment, useRef } from 'react';
+import { useRouter } from 'next/router';
 import { Dialog, Transition } from '@headlessui/react';
 
 export default function BundleModal({ open, bundle, setOpen }) {
   const cancelButtonRef = useRef();
+  const router = useRouter();
+  const close = () => {
+    router.push('/', undefined, { shallow: true });
+    setOpen(false);
+  };
 
   return (
     <Transition show={open} as={Fragment}>
@@ -13,7 +19,7 @@ export default function BundleModal({ open, bundle, setOpen }) {
         className="fixed z-10 inset-0 overflow-y-auto"
         initialFocus={cancelButtonRef}
         open={open}
-        onClose={setOpen}
+        onClose={ close }
       >
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child
@@ -55,7 +61,7 @@ export default function BundleModal({ open, bundle, setOpen }) {
                 <button
                   type="button"
                   className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => setOpen(false)}
+                  onClick={ close }
                   ref={cancelButtonRef}
                 >
                   Close
