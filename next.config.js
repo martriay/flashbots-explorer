a85fc2cc4294c40b7d5f170558ae9a157b39080d
ryry79261 <ryanjnoble@gmail.com>
2021-06-29 12:38:44 +0200

    Fixing deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
+  target: "serverless",
   env: {
     NEXT_PUBLIC_INFURA_ID: process.env.NEXT_PUBLIC_INFURA_ID,
     FLASHBOTS_API_URL: process.env.FLASHBOTS_API_URL
